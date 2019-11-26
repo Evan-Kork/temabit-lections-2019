e46d678730a4c19b580c02976d594028e0a2fd5d
NCrys <roma.ne.sobaka@gmail.com>
2019-11-26 16:33:12 +0200

    3task

--- 20-kostileckiy-roman/dz-9/3task.js
+++ 20-kostileckiy-roman/dz-9/3task.js
@@ -1,5 +1,15 @@
-function fib(n) {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-  }
-var number = prompt("Ennter the number")
-alert(fib(number))
+var FibNum = [0, 1];
+function fibonachi(num) {
+    if (num == 0) {
+        return FibNum[0];
+    } else if (num == 1) {
+        return FibNum[1];
+    } else if (!FibNum[num]) {
+        FibNum[num] = fibonachi(num - 2) + fibonachi(num - 1);
+    };
+    return FibNum[num];
+}
+fibonachi(100);
+for (i = 0; i < FibNum.length; i++) {
+    document.write(FibNum[i] + "</br>");
+}
