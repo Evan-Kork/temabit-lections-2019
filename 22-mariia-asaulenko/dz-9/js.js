8c8be9956501360c513b5e6c2b9b0957ae61dcbb
masaulenko <50613002+masaulenko@users.noreply.github.com>
2019-11-26 10:07:25 +0200

    Update js.js

--- 22-mariia-asaulenko/dz-9/js.js
+++ 22-mariia-asaulenko/dz-9/js.js
@@ -5,11 +5,11 @@
 let n;
 let summ = 0;
 while (!n || n <= 0) {
-    n = prompt('Введите число', 'на этом месте должно быть число');
+     n = Number(prompt('Введите число', 'на этом месте должно быть число'));
 };
 for (let i = 1; i <= n; i++) {
     summ = summ + i;
-}
+};
 alert(`сумма от 1 до ${n} равна ${summ}`);
 
 // дз2 вывести табличку умножения через фор и вайл(2 варианта). Оформить в ввиде таблицы с границами
@@ -21,8 +21,8 @@ for (let i = 1; i < 11; i++) {
         let mult = i * j;
         document.write("<tr><td>" + i + "</td><td>" + '*' + "</td><td>" + j + "</td><td>" + "=" + "</td><td>" + mult + "</td><tr>")
     }
-}
-document.write("</table>")
+};
+document.write("</table>");
 document.write("<table>");
 i = 1;
 while (i < 11) {
@@ -34,9 +34,8 @@ while (i < 11) {
         j++;
     }
     i++;
-}
-document.write("</table></div>")
-
+};
+document.write("</table></div>");
 // дз3 (используя функцию)  вывести числа фибоначи, до 100 через рекурсию
 
 let numFib = [0, 1];
@@ -44,13 +43,13 @@ let numFib = [0, 1];
 function fibonachi(num) {
     if (num == 0) {
         return numFib[0];
-    }
+    };
     if (num == 1) {
         return numFib[1];
-    }
+    };
     if (!numFib[num]) {
         numFib[num] = fibonachi(num - 2) + fibonachi(num - 1);
-    }
+    };
     return numFib[num];
 }
 
