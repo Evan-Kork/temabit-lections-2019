a7c95d4219b023f80b389335c1def2078630efa7
tryguba <trigyba.oleg@gmail.com>
2019-11-28 10:34:19 +0200

    added while

--- 01-oleh-tryhuba/dz-9/index.js
+++ 01-oleh-tryhuba/dz-9/index.js
@@ -1,28 +1,52 @@
+/*
+1. Через діалог promt запросити число N. Якщо число N не більше нуля, то вивести alert -
+"Ви ввели невірне число, повторіть!". Після закриття цього alert - знову з'явиться перший діалог promt.
+Така послідовність діалогів буде до тих пір, поки будемо вводити число не більше нуля.
+Якщо ввели вірне число - вивести через alert суму (от 1 до N).
+
+2. Вивести табличку множення від 1 до 10 за допомогою циклу for та while(два варіанта).
+Таблицю оформити стилями.
+
+3. Використовуючи функції в JS, вивести послідовність чисел Фібоначчі, останє число не більше 100.
+*/
+
+
 let app = document.querySelector('.app');
 // ========================= сума чисел ===============================
 let sum = 0;
 let count = +prompt('введіть число більше 0');
-do {
-	if (count <= 0) {
+if (count <= 0 || isNaN(count)) {
+	do {
+		alert('введіть число більше 0');
 		count = +prompt('введіть число більше 0');
-	}
+		if (count>=0 && !isNaN(count)){
+		
+		}
+	} while (count <= 0 || isNaN(count))
+}
+else {
 	for (let i = 1; i <= count; i++) {
 		sum += i;
 	}
-} while (count <= 0);
-alert(`Сума = ${sum}`);
+	alert(`Сума = ${sum}`);
+}
+
+
 // ========================= числа Фібоначчі ===============================
-let num = +prompt('введіть число Фібоначчі');
-const fib = num => {
-	if (num <= 1) {
-		return num;
+const fib = (max) => {
+	const result = [0, 1];
+	let val;
+	for (let i = 1; ; i++) {
+		val = result[i] + result[i - 1];
+		if (val <= max)
+			result.push(val);
+		else break;
 	}
-	else {
-		return fib(num - 1) + fib(num - 2);
-	}
+	return result;
 };
-alert(fib(num));
-// ========================= табличка множення ===============================
+console.log(fib(100));
+
+// ========================= табличка множення for ===============================
 for (let i = 1; i < 10; i++) {
 	let table = document.createElement("table");
 	table.setAttribute('border', '1px solid');
@@ -37,3 +61,26 @@ for (let i = 1; i < 10; i++) {
 	}
 	app.appendChild(table);
 }
+
+// ========================= табличка множення while ===============================
+let app2 = document.querySelector('.app2');
+let i = 1;
+let k = 1;
+while (i < 10) {
+	let table2 = document.createElement("table");
+	table2.setAttribute('border', '1px solid');
+	while (k < 10) {
+		table2.innerHTML += `<tr>
+										<td>${i}</td>
+										<td> * </td>
+										<td>${k}</td>
+										<td> = </td>
+										<td>${i * k}</td>
+									</tr>`;
+		k++;
+	}
+	app2.appendChild(table2);
+	k = 1;
+	i++;
+}
+
