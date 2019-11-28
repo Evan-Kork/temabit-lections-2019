e426019757c1744d64610534d6ada59ffcb1d4c1
vaskivnyuk <m.vaskivnyuk@gmail.com>
2019-11-28 23:52:38 +0200

    modify functions

--- 34-mykhailo-vaskivnyuk/dz-10/script.js
+++ 34-mykhailo-vaskivnyuk/dz-10/script.js
@@ -2,61 +2,50 @@
 
 'use strict'
 
-function removeValueOne(array, value, offset = -1) {
-	offset = array.lastIndexOf( value, offset );
-	if ( ~offset ) array.splice( offset, 1 );
-	return offset;
-}
-
-function removeValueAll(array, value) {
-	let offset = 0;
-	do {
-		offset = removeValueOne(array, value, offset - 1)
-	} while ( offset > 0 )
+function removeValue(array, value) {
+	array.reduceRight(
+		(prev, item, index) => array.splice( index, item === value )
+		0
+	);
 }
 
 function removeValues(array, ...values) {
-	values.forEach( value => removeValueAll(array, value) ); 
+	values.forEach( value => removeValue(array, value) ); 
 }
 
 function unique(array) {
-	return array.filter( (value, index, array) => ! array.includes( value, index + 1 ) );
+	return array.filter( (value, index) => ! array.includes( value, index + 1 ) );
 }
 
 function difference(array1, array2){
 	return array1.filter( value => ! array2.includes( value ) );
 };
 
-//------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 let test_array = ["first", "second", "second", "third", "first"];
 console.log('array: ' + test_array);
 let array = [];
+let result = [];
 
-//-- TASK 1.1 -----------------------------------------------------------------
+//-- TASK 1 -------------------------------------------------------------------
 array = test_array.slice();
-removeValueOne(array, "first");
-console.log('task 1.1 removeValueOne(array, "first"): ' + array);
-
-//-- TASK 1.2 -----------------------------------------------------------------
-array = test_array.slice();
-removeValueAll(array, "first");
-console.log('task 1.2 removeValueAll(array, "first"): ' + array);
+removeValue(array, "first");
+console.log('task 1 removeValueAll(array, "first"): ' + array);
 
 //-- TASK 2 -------------------------------------------------------------------
 array = test_array.slice();
 removeValues(array, "first", "third", "forth");
 console.log('task 2 removeValues(array, "first", "third", "forth"): ' + array);
 
-//-- TASK 3 -----------------------------------------------------------------
+//-- TASK 3 -------------------------------------------------------------------
 array = test_array.slice();
-let result = unique(array);
+result = unique(array);
 console.log("task 3 unique(array): " + result);
 
-//-- TASK 4 -----------------------------------------------------------------
+//-- TASK 4 -------------------------------------------------------------------
 array = test_array.slice();
-let array2 = ["second", "forth"]
-result = difference(array, array2);
+result = difference(array, ["second", "forth"]);
 console.log('task 4 difference(array, ["second", "forth"]): ' + result);
 
-//-----------------------------------------------------------------
+//-----------------------------------------------------------------------------
 alert("done");
