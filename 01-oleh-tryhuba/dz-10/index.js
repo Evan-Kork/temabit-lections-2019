12aa6f8eb4c47dbfd764fc55fe908c7c31d4a3f2
tryguba <trigyba.oleg@gmail.com>
2019-12-01 11:54:59 +0200

    fixed the bugs

--- 01-oleh-tryhuba/dz-10/index.js
+++ 01-oleh-tryhuba/dz-10/index.js
@@ -1,36 +1,36 @@
-
 // ================= 1 ============================
-const removeElement = (arr, num) => arr.filter(el => el != num);
+const removeElement = (arr, num) => arr.filter(el => el !== num);
 const array = ['Kiev', 'Beijing', 'Lima', 'Saratov'];
 console.log(removeElement(array, 'Saratov'));
 
 // ================= 2 =============================
-const removeElements = (arr, ...arr2) => {
-	for (let i = 0; i < arr2.length; i++) {
-		for (let j = 0; j < arr2.length; j++) {
-			const count = arr.indexOf(arr2[i]);
-			if (count !== -1) {
-				arr.splice(count, 1);
-			}
+function removeElements() {
+	const args = Array.from(arguments);
+	args.forEach((value, index) => {
+		if (~args[0].indexOf(arguments[index])) {
+			args[0].splice(args[0].indexOf(arguments[index]), 1);
 		}
-	}
-	return arr
-};
+	});
+}
+
 const array22 = [1, 2, 3, 4, 5, 6, 7];
 removeElements(array22, 5, 1, 6);
 console.log(array22);
 
 // ================= 3 =============================
-const unique = arr => [...new Set(arr)];
+function unique(arr) {
+	let result = [];
+	for (let str of arr) {
+		if (!result.includes(str)) {
+			result.push(str);
+		}
+	}
+	return result;
+}
 const result = unique(['top', 'bottom', 'top', 'left']);
 console.log(result);
-
 // ================= 4 =============================
-const difference = (arr1, arr2) => {
-	return arr1.filter(item => {
-		return false === arr2.includes(item, 0);
-	});
-};
+const difference = (arr1, arr2) => arr1.filter(item => !arr2.includes(item));
 const array1 = [7, -2, 10, 5, 0];
 const array2 = [0, 10];
 const res = difference(array1, array2);
