a0982ab62e9929f753c2b257622fad91db9b4a47
OlhaRedko <olga@words.com.ua>
2019-12-02 15:42:00 +0200

    fixed script.js

--- 24-olga-redko/dz-10/script.js
+++ 24-olga-redko/dz-10/script.js
@@ -8,14 +8,15 @@ function removeElement(array, item) {
 }
 
 //task 2
-function removeElements(array, ...items) {
-  for (let i = 0; i < items.length; i++) {
-    let index = array.indexOf(items[i]);
+function removeElements() {
+  let arr = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    let index = arr.indexOf(arguments[i]);
     if (index !== -1) {
-      array.splice(index, 1);
+      arr.splice(index, 1);
     }
   }
-  return array;
+  return arr;
 }
 
 //task 3
