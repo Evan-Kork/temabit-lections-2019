b3c208cb8b6fd86697d07a25722295727d4df955
Evgenii Khvorostyuk <evgeniykhvorostyuk@gmail.com>
2019-12-02 16:16:35 +0200

    Fibonacci func & multiplication table (via while loop)

--- 11-evgenii-khvorostyuk/dz-9/index.js
+++ 11-evgenii-khvorostyuk/dz-9/index.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const promptPositiveNumber = () => {
-  let num = 0
+  let num = 0;
   do {
-    num = prompt('Please, enter positive number: ')
-    if (num === null || num === '') break
-    num = parseInt(num)
-    let num2 = 0
+    num = prompt('Please, enter positive number: ');
+    if (num === null || num === '') break;
+    num = parseInt(num);
+    let num2 = 0;
 
     while (num2 < num) {
-      console.log(num2)
-      num2++
+      console.log(num2);
+      num2++;
     }
   }
   while (num <= 0 || isNaN(num))
@@ -30,3 +32,40 @@ const table = () => {
 }
 
 table()
+
+// const tableWhile = () => { 
+//   if (document.querySelector('#multiplication_table tbody') === null) return;
+//   let draw = '';
+//   let n = 1;
+//   while (n <= 10) {
+//     draw += `<td><table><tbody>`;
+//     let i = 1;
+//     while (i <= 10) {
+//       draw += `<tr><td>${n}x${i} = ${n*i}</td></tr>`;
+//       i++;
+//     }
+//     draw += `</tbody></table></td>`;
+//     let tr = document.querySelector(`#multiplication_table tbody #${n <= 5 ? 'top' : 'bottom'}`);
+//     tr.innerHTML = draw;
+//     if (n === 5) draw = '';
+//     n++;
+//   }
+// }
+
+// tableWhile()
+
+function fib(n) {
+  if (n <= 1) return n;
+  else return fib(n - 1) + fib(n - 2);
+}
+
+function showFibSequence(n) {
+  let sequence = []
+  for (let i = 0; i <= n; i++) {
+    sequence.push(fib(i))
+  }
+  console.log('fib -> ', sequence.toString());
+  return sequence.toString();
+}
+
+showFibSequence(11);
