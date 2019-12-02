2b2202aececba8bce6a5dc53cd97c4538fe9fe7e
oleg <fod@ua.fm>
2019-12-03 00:15:26 +0200

    dz-10.

--- 02-oleg-ferenets/dz-10/js1.js
+++ 02-oleg-ferenets/dz-10/js1.js
@@ -27,16 +27,6 @@ console.log("Task 2")
 
 const array3 = [1, 2, 3, 4, 5, 6, 7];
 
-//function removeElements(array, ...items) {
-//    for (item of items) {
-//        let index = array.indexOf(item);
-//        if (index != -1) {
-//            array.splice(index, 1);
-//        }
-//    }
-//    return array;
-//}
-
 function removeElements(array) {
     for (let i = 0; i < arguments.length; i++) {
         let index = array.indexOf(arguments[i]);
