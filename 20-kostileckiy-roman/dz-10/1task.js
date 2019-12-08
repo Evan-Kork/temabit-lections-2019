3e2b0a1671b1fd80343c590620193d78cae6d8cb
NCrys <roma.ne.sobaka@gmail.com>
2019-12-08 18:07:49 +0200

    JS fixes

--- 20-kostileckiy-roman/dz-10/1task.js
+++ 20-kostileckiy-roman/dz-10/1task.js
@@ -1,15 +1,13 @@
-// https://codesandbox.io/s/red-fog-x5j4p?expanddevtools=1&fontsize=14&hidenavigation=1&theme=dark
+// https://codesandbox.io/s/amazing-euclid-z04eh?expanddevtools=1&fontsize=14&hidenavigation=1&theme=dark
 
 //task1
 var myArrCountry = ['Ukraine','Belarus', 'Poland', 'Russia', 'Germany', 'France', 'Italy', 'Spain']
 var myArrNum = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0]
-function removeItem(array, item){
-    for(let i =0; i <array.length; i++){
-        let a = array.indexOf(item);
-        if(a !== -1){
-            array.splice(a,1);
-            i--;
-        }else{break}
+function removeItem(array, item) {
+    for (let i = 0; i < array.length; i++) {
+      if (array.indexOf(item) !== -1) {
+        array.splice(array.indexOf(item),1);
+      }
     }
 }
 removeItem(myArrNum, 0);
@@ -18,16 +16,12 @@ console.log(myArrNum);
 console.log(myArrCountry);
 //task2
 function removeItems(array, ...items){
-    for( let i = 0; i <items.length; i++){
-        while(true){
-            let a = array.indexOf(items[i])
-            if(a !== -1 ){
-                array.splice(a,1);
-                i--;
-            }else{break}
+    for (let i = 0; i < array.length; i++) {
+        if (array.indexOf(items) !== -1) {
+          array.splice(array.indexOf(items),1);
         }
+      }
     }
-}
 removeItems(myArrNum, 2,5,8,13);
 removeItems(myArrCountry, 'France', 'Italy');
 console.log(myArrNum);
@@ -37,9 +31,8 @@ function noDublicates(array) {
     let noDublicatesArray = array.slice();
     for (var i = 0; i < noDublicatesArray.length; i++) {
       while (true) {
-        let a = noDublicatesArray.indexOf(noDublicatesArray[i], i + 1);
-        if (a !== -1) {
-         noDublicatesArray.splice(a, 1);
+        if (noDublicatesArray.indexOf(noDublicatesArray[i], i + 1) !== -1) {
+         noDublicatesArray.splice(noDublicatesArray.indexOf(noDublicatesArray[i]),1);
           i--;
         } else {
           break;
@@ -48,9 +41,9 @@ function noDublicates(array) {
     }
     return noDublicatesArray;
   }
-const randomArr = [2,2,3,32,2,3,3,4,4,52,2,3,3,4,4,5,4,4,5]
-var noDubArr = noDublicates(randomArr)
-console.log(noDubArr)
+  const randomArr = [2,2,3,32,2,3,3,4,4,52,2,3,3,4,4,5,4,4,5]
+  var noDubArr = noDublicates(randomArr)
+  console.log(noDubArr)
 //task4
 const array1 = [82, 92, 18, 13, 22,123,12,31,3222,1233,131,123,9877,76,14]
 const array2 = [123,13,180,0,10,222,332,76,131,18]
