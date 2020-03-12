b0b4f4e7603497a5cde020d5f968fae04f565c42
vaskivnyuk <m.vaskivnyuk@gmail.com>
2020-03-13 01:44:21 +0200

    events

---
+++ 34-mykhailo-vaskivnyuk/dz-17/index.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+const path = require("path");
+const ScanDir = require("./ScanDir");
+
+const dir_name_from = "src_2";
+const dir_name_to = "src_1";
+const files = new Map();
+const scan_dir = new ScanDir();
+
+scan_dir.on("fileOpen", open_file_for_write);
+scan_dir.on("fileRead", write_data_to_file);
+scan_dir.on("fileClose", close_file);
+
+scan_dir.scan(dir_name_from);
+
+//---------------------------------------------------------------------
+
+function open_file_for_write(err, file_name, path_to_file, file_handler = null, read_file = null) {
+
+	if ( err ) {
+		console.log("Error: " + err.message);
+		return;
+	}
+
+	path_to_file = path.join(dir_name_to, path_to_file);
+	fs.mkdirSync(path_to_file, { recursive: true });
+	const file_path = path.join(path_to_file, file_name);
+	
+	fs.open(file_path, "w", (err, handler) => {
+
+		if ( err ) {
+			console.log("Error: " + err.message);
+			read_file(false);
+			return;
+		}
+
+		 files.set(file_handler.fd, handler);
+		 console.log("Open file for write: " + file_name + file_handler.fd);
+		 read_file(true);
+	});
+}
+
+//---------------------------------------------------------------------
+
+function write_data_to_file(err, file_handler, bytesRead = null, buffer = null, read_next = null) {
+	
+	const handler = files.get(file_handler.fd);
+	
+	if ( err ) {
+		console.log("Error: " + err.message);
+		close_file(file_handler);
+		return;
+	}
+
+	console.log("Write data to file: " + handler);
+	console.log(handler);
+
+	fs.write(handler, buffer, 0, bytesRead, err => {
+		if ( err ) {
+			console.log("Error: " + err.message);
+			close_file(file_handler);
+			read_next(false);
+			return;
+		}
+		if ( bytesRead ) read_next(true)
+	});
+}
+
+//---------------------------------------------------------------------
+
+function close_file() {
+		
+	const handler = files.get(file_handler.fd);
+	fs.closeSync(handler);
+	files.delete(file_handler.fd);
+
+}
+
+//---------------------------------------------------------------------
