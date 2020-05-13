c24eea1820da0aeae6010c5349b63beb2fa9a17b
NCrys <roma.ne.sobaka@gmail.com>
2020-05-13 17:55:57 +0300

    REF.components.Partners.MultiCarousel

--- 20-kostileckiy-roman/dz-14 CourseProject/src/components/tabsPartners/multicaRousel/multiCarousel.js
+++ 20-kostileckiy-roman/dz-14 CourseProject/src/components/tabsPartners/multicaRousel/multiCarousel.js
@@ -5,52 +5,36 @@ import CarouselItem from "react-bootstrap/CarouselItem";
 import Card from "react-bootstrap/Card";
 import styles from './multiCarousel.module.css'
 import CardGroup from "react-bootstrap/CardGroup";
-import RozetkaLogo from './../../../resources/partners/rozetka_logo.png'
-import AutoluxLogo from './../../../resources/partners/autolux_logo.png'
-import FozzyLogo from './../../../resources/partners/fozzy_logo.png'
-import ElmirLogo from './../../../resources/partners/elmir_logo.png'
-import ParfumsLogo from './../../../resources/partners/parfums_logo.png'
-import PromLogo from './../../../resources/partners/prom_logo.png'
-import RingoLogo from './../../../resources/partners/ringo_logo.png'
-import ThrashLogo from './../../../resources/partners/thrash_logo.png'
-
+import {createStore} from "redux";
+import {tabsAndPartner} from "../../../redux/tabsAndPartnerReduser";
 
 class MultiCarousel extends Component {
+    constructor(props) {
+        super(props);
+        this.store = createStore(tabsAndPartner).getState().MULTICAROUSEL
+        this.logoPackOne = this.store.PARTNERS_LOGO_1.map(element =>
+                <Card style={{ width: '18rem' }} className={styles.cardsImg}>
+                    <Card.Img variant="top" src={element}/>
+                </Card>
+        )
+        this.logoPackTwo = this.store.PARTNERS_LOGO_2.map(element =>
+            <Card style={{ width: '18rem' }} className={styles.cardsImg}>
+                <Card.Img variant="top" src={element}/>
+            </Card>
+        )
+    }
     render() {
         return (
             <Container>
                     <Carousel controls={false}>
                         <CarouselItem>
                             <CardGroup className={styles.cardsgroup}>
-
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={RozetkaLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={AutoluxLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={FozzyLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={ElmirLogo} />
-                                </Card>
+                                {this.logoPackOne}
                             </CardGroup>
                         </CarouselItem>
                         <CarouselItem>
                             <CardGroup className={styles.cardsgroup}>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={ParfumsLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={PromLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={RingoLogo} />
-                                </Card>
-                                <Card style={{ width: '18rem' }} className={styles.cardsImg} >
-                                    <Card.Img variant="top" src={ThrashLogo} />
-                                </Card>
+                                {this.logoPackTwo}
                             </CardGroup>
                         </CarouselItem>
                     </Carousel>
--- 20-kostileckiy-roman/dz-14 CourseProject/src/redux/tabsAndPartnerReduser.js
+++ 20-kostileckiy-roman/dz-14 CourseProject/src/redux/tabsAndPartnerReduser.js
@@ -1,6 +1,14 @@
 import Locations from "../resources/ico-location.png";
 import Calculator from "../resources/ico-calculator.png";
 import partners from '../resources/partners.png';
+import RozetkaLogo from '../resources/partners/rozetka_logo.png'
+import AutoluxLogo from '../resources/partners/autolux_logo.png'
+import FozzyLogo from '../resources/partners/fozzy_logo.png'
+import ElmirLogo from '../resources/partners/elmir_logo.png'
+import ParfumsLogo from '../resources/partners/parfums_logo.png'
+import PromLogo from '../resources/partners/prom_logo.png'
+import RingoLogo from '../resources/partners/ringo_logo.png'
+import ThrashLogo from '../resources/partners/thrash_logo.png'
 
 const initialState = {
     LOCATION:{
@@ -19,6 +27,20 @@ const initialState = {
     PARTNERS:{
         IMG:partners,
         TEXT:"Партнери",
+    },
+    MULTICAROUSEL:{
+        PARTNERS_LOGO_1:[
+            RozetkaLogo,
+            AutoluxLogo,
+            FozzyLogo,
+            ElmirLogo
+        ],
+        PARTNERS_LOGO_2:[
+            ParfumsLogo,
+            PromLogo,
+            RingoLogo,
+            ThrashLogo
+        ]
     }
 }
 export const tabsAndPartner = (state = initialState, action) =>{
