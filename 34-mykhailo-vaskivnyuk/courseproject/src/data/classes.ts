2ffd2348fb56855f2036f6211bac521442787866
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2020-07-16 21:46:09 +0300

    test decorators

--- 34-mykhailo-vaskivnyuk/courseproject/src/data/classes.ts
+++ 34-mykhailo-vaskivnyuk/courseproject/src/data/classes.ts
@@ -2,24 +2,43 @@ import {
     validate, validateOrReject,
     Contains, IsInt, Length, IsEmail, IsFQDN, IsDate, Min, Max} from "class-validator";
 
+// https://medium.com/devschacht/a-minimal-guide-to-ecmascript-decorators-c7348f61ba24
+
+function myDecorator(target: any, property: any, descriptor?: any) {
+    console.log(target);
+    console.log(property);
+
+    // if (!descriptor) return descriptor;
+
+    console.log(descriptor);
+    return {...descriptor};
+}
+
+function myClassDecorator(target: any) {
+    console.log(target);
+}
+
+// @myClassDecorator
 export class Branch {
-        delivery_branch_id: number;
-        @IsInt()
-        number: number;
-        adress: string;
-        public: {
-            navigation_ua: string,
-        };
-        shedule_description: string;
-        services: Data.Services;
-        lat: number;
-        lng: number;
-        max_weight: number;
-        photos: string[];
-        locality: string;
+    delivery_branch_id: number;
+    @IsInt()
+    number: number;
+    @myDecorator
+    adress: string;
+    public: {
+        navigation_ua: string,
+    };
+    shedule_description: string;
+    services: Data.Services;
+    lat: number;
+    lng: number;
+    max_weight: number;
+    photos: string[];
+    locality: string;
 }
 
 export class BranchClass extends Branch {
+    @myDecorator
     get navigation_ua() {
         return this.public.navigation_ua;
     }
